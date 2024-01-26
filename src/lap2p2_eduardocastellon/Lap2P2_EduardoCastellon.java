8860ff1d0280d2fbff454a7919cc456103c6cd3e
ediiee19 <castelloneduardo2005@gmail.com>
2024-01-26 14:20:35 -0600

    fin de login

--- src/lap2p2_eduardocastellon/Lap2P2_EduardoCastellon.java
+++ src/lap2p2_eduardocastellon/Lap2P2_EduardoCastellon.java
@@ -14,10 +14,61 @@ import java.util.Scanner;
 public class Lap2P2_EduardoCastellon {
 
     public static void main(String[] args) {
-        Scanner entrada= new Scanner(System.in);
-        ArrayList <Usuarios> usuarios= new ArrayList();
-        
-        
+        Scanner entrada = new Scanner(System.in);
+        ArrayList<Usuarios> usuarios = new ArrayList();
+
+        usuarios.add(new Usuarios("EduardoC", "Tinker100", "e"));//estudiante
+        usuarios.add(new Usuarios("JaveriL", "general300", "p"));//profesor
+        usuarios.add(new Usuarios("Rikardo,M", "chinfu20", "b"));//bibliotecario
+
+        boolean menuLogIn = true;
+        while (menuLogIn) {
+            System.out.println("---Log-In---\n"
+                    + "Ingresar nombre de usuario: ");
+            String nombre = entrada.nextLine();
+            System.out.println("Ingresar contraseña: ");
+            String contra = entrada.nextLine();
+
+            boolean usuarioEncon = false;
+            boolean contraEncon = false;
+            String tipo = "e";
+            for (Usuarios usuario : usuarios) {
+                if (nombre.equals(usuario.getNombre())) {
+                    usuarioEncon = true;
+                    if (contra.equals(usuario.getContra())) {
+                        contraEncon = true;
+                        tipo = usuario.getTipo();
+                    }
+                }
+            }
+            if (usuarioEncon && contraEncon) {
+                if (tipo.equals("e")) {
+                    menuEstudiante();
+                }
+                if (tipo.equals("p")) {
+                    menuProfesor();
+                }
+                if (tipo.equals("b")) {
+                    menuBibliotecario();
+                }
+            } else {
+                usuarioEncon = false;
+                contraEncon = false;
+                System.out.println("Usuario o contraseña incorrecta");
+            }
+        }//fin del while
     }
     
+    public static void menuEstudiante(){
+        System.out.println("Bienvenido al menu de estudiante");   
+    }
+    
+    public static void menuProfesor(){
+        System.out.println("Bienvenido al menu de profesor");   
+    }
+    
+    public static void menuBibliotecario(){
+        System.out.println("Bienvenido al menu de bibliotecario");
+    }
+
 }
--- src/lap2p2_eduardocastellon/Usuarios.java
+++ src/lap2p2_eduardocastellon/Usuarios.java
@@ -9,5 +9,37 @@ package lap2p2_eduardocastellon;
  * @author caste
  */
 public class Usuarios {
-    
+    private String nombre;
+    private String contra;
+    private String tipo;
+
+    public Usuarios(String nombre, String contra, String tipo) {
+        this.nombre = nombre;
+        this.contra = contra;
+        this.tipo = tipo;
+    }
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        this.nombre = nombre;
+    }
+
+    public String getContra() {
+        return contra;
+    }
+
+    public void setContra(String contra) {
+        this.contra = contra;
+    }
+
+    public String getTipo() {
+        return tipo;
+    }
+
+    public void setTipo(String tipo) {
+        this.tipo = tipo;
+    }
 }
