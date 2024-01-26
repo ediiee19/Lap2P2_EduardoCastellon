6054f2af3f70f241246a68f1017ce90344bb0ef2
ediiee19 <castelloneduardo2005@gmail.com>
2024-01-26 13:37:00 -0600

    inicio de lab

---
+++ src/lap2p2_eduardocastellon/Lap2P2_EduardoCastellon.java
@@ -0,0 +1,23 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package lap2p2_eduardocastellon;
+
+import java.util.ArrayList;
+import java.util.Scanner;
+
+/**
+ *
+ * @author caste
+ */
+public class Lap2P2_EduardoCastellon {
+
+    public static void main(String[] args) {
+        Scanner entrada= new Scanner(System.in);
+        ArrayList <Usuarios> usuarios= new ArrayList();
+        
+        
+    }
+    
+}
